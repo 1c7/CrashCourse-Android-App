7ab792dcfb0f1342d1c894e7f0c0de67ccb95c68
1c7 <guokrfans@gmail.com>
2017-08-11 14:47:22 +0800

    finally

--- src/Api.js
+++ src/Api.js
@@ -2,11 +2,11 @@
 // API 地址全都放这里
 // 这样就不用到处改了。
 
-var root_url = 'http://106.75.130.23:12000';
+var root_url = 'http://crashcourse.myherego.com';
 
 export default Api_list = {
   newest_url: root_url + `/api/newest`,
   serie_url: root_url + "/api/series",
-  single_serie_url: root_url + "/api/serie",
+  single_serie_url: root_url + "/api/serie/",
   note_url: root_url + "/about_cc",
 }
